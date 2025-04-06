16d4e605641885b837337924856550421b0b5148
aviel3899 <aviel3899@gmail.com>
2025-04-06 19:45:45 +0300

    firebase and google cloud API connection

--- WePool/app/build.gradle.kts
+++ WePool/app/build.gradle.kts
@@ -81,4 +81,5 @@ dependencies {
     implementation(platform(libs.firebase.bom))
     implementation(libs.firebase.auth)
     implementation(libs.firebase.firestore)
+    implementation(libs.material)
 }
