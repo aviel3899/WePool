64058497e2b5ecdb688a63f86976a8bc8426e375
Elior <elioruzan06@gmail.com>
2025-05-04 11:56:23 +0300

    my latest version

--- WePool/app/src/main/java/com/wepool/app/data/model/ride/PickupStop.kt
+++ WePool/app/src/main/java/com/wepool/app/data/model/ride/PickupStop.kt
@@ -5,6 +5,6 @@ import com.wepool.app.data.model.common.LocationData
 data class PickupStop(
     val location: LocationData = LocationData(),
     val passengerId: String = "",
-    val pickupTime: String? = null,    // שעת איסוף (HH:mm)
-    val dropoffTime: String? = null    // שעת הורדה (HH:mm)
+    val pickupTime: String? = null,    // שעת איסוף(HH:mm)
+    val dropoffTime: String? = null    // שעת הורדה(HH:mm)
 )
